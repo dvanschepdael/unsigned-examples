c3341dbd8bd4f4e977b6194623cad403b4c25f98
dvans <david.vanschepdael@outlook.com>
2026-09-10 13:03:39 +0200

    refactor: express match flow with UStateGraph and ULevel

--- versus-fighting/src/versus_match.h
+++ versus-fighting/src/versus_match.h
@@ -1,8 +1,10 @@
 #ifndef VERSUS_MATCH_H
 #define VERSUS_MATCH_H
 
-#include "display/viewport/viewport.h"
+#include "core/state/state_graph.h"
 #include "input/input.h"
+#include "level/level_definition.h"
+#include "level/level_runtime.h"
 
 #include "versus_fighter.h"
 
@@ -11,10 +13,21 @@ typedef enum VersusRoundPhase {
     VERSUS_ROUND_FIGHT,
     VERSUS_ROUND_OUTRO,
     VERSUS_MATCH_OVER,
+    VERSUS_ROUND_PHASE_COUNT,
 } VersusRoundPhase;
 
 typedef struct VersusMatch {
     VersusFighter fighters[2];
+
+    /* Match/round flow uses the same generic state graph primitive as fighters. */
+    UStateGraph state_graph;
+    UStateGraphNode state_root;
+    UStateGraphNode state_nodes[VERSUS_ROUND_PHASE_COUNT];
+    UStateGraphNodeContainer state_children;
+    UStateGraphTransition state_events[VERSUS_ROUND_PHASE_COUNT];
+    UStateGraphTransitionContainer state_event_container;
+
+    const UInputManager *frame_input;
     VersusRoundPhase phase;
     u16 phase_frames;
     u16 round_frames_remaining;
@@ -25,10 +38,11 @@ typedef struct VersusMatch {
     u8 winner;
 } VersusMatch;
 
+extern const ULevelDefinition VERSUS_ARENA_LEVEL;
+
 bool versus_match_init(VersusMatch *match, u16 refresh_rate);
 void versus_match_start(VersusMatch *match);
-void versus_match_tick(VersusMatch *match, const UInputManager *input);
-void versus_match_render(VersusMatch *match, const UViewport *viewport);
+void versus_match_update(VersusMatch *match, const UInputManager *input);
 bool versus_match_finished(const VersusMatch *match);
 
 #endif
