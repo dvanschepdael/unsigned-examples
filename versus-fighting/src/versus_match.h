dce1f39447c27a85ebf4e9af92ea1b3098a90451
dvans <david.vanschepdael@outlook.com>
2026-09-10 13:19:19 +0200

    refactor: simplify match public state and remove arena ownership

--- versus-fighting/src/versus_match.h
+++ versus-fighting/src/versus_match.h
@@ -1,12 +1,10 @@
 #ifndef VERSUS_MATCH_H
 #define VERSUS_MATCH_H
 
-#include "core/state/state_graph.h"
 #include "input/input.h"
-#include "level/level_definition.h"
-#include "level/level_runtime.h"
 
 #include "versus_fighter.h"
+#include "versus_state.h"
 
 typedef enum VersusRoundPhase {
     VERSUS_ROUND_INTRO = 0,
@@ -17,32 +15,31 @@ typedef enum VersusRoundPhase {
 } VersusRoundPhase;
 
 typedef struct VersusMatch {
-    VersusFighter fighters[2];
+    VersusFighter fighters[VERSUS_PLAYER_COUNT];
 
-    /* Match/round flow uses the same generic state graph primitive as fighters. */
-    UStateGraph state_graph;
-    UStateGraphNode state_root;
+    /* Round flow uses the same small UStateGraph adapter as fighters. */
+    VersusStateMachine states;
     UStateGraphNode state_nodes[VERSUS_ROUND_PHASE_COUNT];
-    UStateGraphNodeContainer state_children;
-    UStateGraphTransition state_events[VERSUS_ROUND_PHASE_COUNT];
-    UStateGraphTransitionContainer state_event_container;
+    UStateGraphTransition state_transitions[VERSUS_ROUND_PHASE_COUNT];
 
     const UInputManager *frame_input;
-    VersusRoundPhase phase;
     u16 phase_frames;
     u16 round_frames_remaining;
     u16 refresh_rate;
-    u8 rounds_won[2];
+    u8 rounds_won[VERSUS_PLAYER_COUNT];
     u8 round_number;
     u8 hitstop_frames;
     u8 winner;
 } VersusMatch;
 
-extern const ULevelDefinition VERSUS_ARENA_LEVEL;
-
 bool versus_match_init(VersusMatch *match, u16 refresh_rate);
 void versus_match_start(VersusMatch *match);
 void versus_match_update(VersusMatch *match, const UInputManager *input);
+
+VersusRoundPhase versus_match_phase(const VersusMatch *match);
 bool versus_match_finished(const VersusMatch *match);
+
+/* Called by the arena's level callback when Unsigned reports an attacker/target pair. */
+void versus_match_resolve_hit(VersusMatch *match, UActor *attacker, UActor *target);
 
 #endif
