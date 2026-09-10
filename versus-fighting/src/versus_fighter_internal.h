70d1577c10b52dabfc530db522a51497be1b1387
dvans <david.vanschepdael@outlook.com>
2026-09-10 13:23:40 +0200

    cleanup: keep fighter internal header private-only

--- versus-fighting/src/versus_fighter_internal.h
+++ versus-fighting/src/versus_fighter_internal.h
@@ -3,11 +3,6 @@
 
 #include "versus_fighter.h"
 
-enum {
-    VERSUS_ATTRIBUTE_HEALTH = 0,
-    VERSUS_ATTRIBUTE_COUNT = 1,
-};
-
 bool versus_fighter_states_init(VersusFighter *fighter);
 void versus_fighter_set_state(VersusFighter *fighter, VersusFighterState state);
 void versus_fighter_play_state_animation(VersusFighter *fighter, VersusFighterState state);
